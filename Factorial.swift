3ab026e282f0a2b5a457346f93413e48d24abb74
Kent Gatera <kent.gatera@stu.ocsb.ca>
2024-05-20 19:24:41 +0000

    Yes

--- Factorial.swift
+++ Factorial.swift
@@ -1,3 +1,4 @@
+import Foundation
 
 //  Factorial.swift
 //
@@ -6,4 +7,50 @@
 //  Version 1.0
 //  Copyright (c) Kent Gatera. All rights reserved.
 //
-//  Change me.
+
+// Factorial class
+final class Factorial {
+    
+    // Private constructor to prevent instantiation.
+    private init() {
+        fatalError("Cannot instantiate")
+    }
+    
+    static func main() {
+        let inputFile = URL(fileURLWithPath: "input.txt")
+        let outputFile = URL(fileURLWithPath: "output.txt")
+        
+        do {
+            let inputString = try String(contentsOf: inputFile, encoding: .utf8)
+            let inputs = inputString.components(separatedBy: .newlines)
+            var outputStrings = [String]()
+            
+            for inputLine in inputs {
+                if let number = Int(inputLine) {
+                    let factorial = RecFactorial(number)
+                    outputStrings.append("\(factorial)")
+                } else {
+                    outputStrings.append("Must be an integer!")
+                }
+            }
+            
+            let outputString = outputStrings.joined(separator: "\n")
+            try outputString.write(to: outputFile, atomically: true, encoding: .utf8)
+            print("Done.")
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+    
+    // Recursive method that gets the factorial of a number.
+    private static func RecFactorial(_ n: Int) -> Int {
+        if n == 0 {
+            return 1
+        } else {
+            return n * RecFactorial(n - 1)
+        }
+    }
+}
+
+// Call the main method to execute
+Factorial.main()
